be63c14e37fff521eece30c60137fb7be3149c3f
thief01 <pst12345@wp.pl>
2025-06-20 11:56:17 +0200

    add taxex and business type

--- backend/Companies/models.py
+++ backend/Companies/models.py
@@ -6,6 +6,14 @@ from django.db import models
 from django.db import models
 
 class Company(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    BUSINESS_TYPE_CHOICES = [
+        ('BA', 'Business activity'),
+        ('LLC', 'Limited liability company'),
+        ('JSC', 'Joint stock company'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    vat = models.BooleanField(default=False)
+    tax_value = models.DecimalField(max_digits=10, decimal_places=2, default=12)
+    company_type = models.CharField(max_length=3, choices=BUSINESS_TYPE_CHOICES, default='BA', verbose_name="Company Type")
 
