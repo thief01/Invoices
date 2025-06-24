59a476658b3df600be14c8972280c922068e5f00
thief01 <pst12345@wp.pl>
2025-06-24 08:21:48 +0200

    add get companies

--- backend/Companies/views.py
+++ backend/Companies/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
@@ -16,5 +17,15 @@ def add_new_compoany(request):
         company.tax_value = request.POST['tax_value']
         company.save()
         return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'})
+
+@login_required
+def get_companies(request):
+    if request.method == "GET":
+        companies =  Company.objects.filter(user = request.user)
+        return JsonResponse({
+            'status': 'success',
+            'companies': list(companies.values('id', 'name', 'vat', 'tax_value', 'company_type'))
+        })
     return JsonResponse({'status': 'error'})
 
