267cf84ec3eacfdf04ef8965d184e297fd61d412
thief01 <pst12345@wp.pl>
2025-06-24 08:04:20 +0200

    add company model and adding

--- backend/Companies/urls.py
+++ backend/Companies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='login'),
+    path('', views.add_new_compoany, name='add'),
 ]
--- backend/Companies/views.py
+++ backend/Companies/views.py
@@ -5,6 +5,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse, JsonResponse
 
+from Companies.models import Company
 
-def index(request):
-    return JsonResponse({'message': 'Hello, World!'})
+
+def add_new_compoany(request):
+    if request.method == "POST":
+        company = Company()
+        company.name = request.POST['name']
+        company.vat = request.POST['vat']
+        company.tax_value = request.POST['tax_value']
+        company.save()
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'error'})
+
