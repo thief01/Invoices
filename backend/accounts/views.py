2732391ac2d22889cd03f215de5f31becb37a655
thief01 <pst12345@wp.pl>
2025-06-30 12:27:22 +0200

    add login page

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,9 @@
 import json
 
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 # Create your views here.
 
 from django.http import HttpResponse, JsonResponse
@@ -12,16 +13,18 @@ from django.views.decorators.http import require_POST
 
 
 def user_login(request):
+
     if request.method == "POST":
-        data = json.loads(request.body)
-        username = data.get('username')
-        password = data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            return JsonResponse({'error': 'Invalid username or password'}, status=401)
-        login(request, user)
-        return JsonResponse({'status': 'ok'})
-    return JsonResponse({'error': 'Method not allowed'}, status=405)
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("index")
+        else:
+            messages.error(request, "Nieprawidłowa nazwa użytkownika lub hasło.")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 
 
