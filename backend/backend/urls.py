8a1cb1df5a52d29f63ac368f02494f1847d78783
thief01 <pst12345@wp.pl>
2025-06-30 12:27:12 +0200

    add index page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from backend import views
 from backend.views import get_csrf_token
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path("accounts/", include('accounts.urls')),
     path('companies/', include('companies.urls')),
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,7 +1,14 @@
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 
 @ensure_csrf_cookie
 def get_csrf_token(request):
     return JsonResponse({'detail': 'CSRF cookie set'})
+
+def index(request):
+    context = {
+        'user': request.user,
+    }
+    return render(request, 'index.html', context)
