9777bbc764ce2d510cafdc034ea66b7f8f16cfdf
thief01 <pst12345@wp.pl>
2025-07-21 13:22:30 +0200

    add fakturki pl

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r9la4lw&kk%ac$cu9=d&3(upa5a37lut114-i5+4=$rf8tjwa!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "fakturki.mimigames.pl", "localhost", "0.0.0.0"]
 
 
 # Application definition
