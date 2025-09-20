1cbd84fb85570ecd155cfbfa43ea1a72ee5c2471
thief01 <pst12345@wp.pl>
2025-09-20 23:42:20 +0200

    add account, urls, forms sketchs

---
+++ backend/accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .models import User
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    class Meta:
+        model = User
+        fields = ['username', 'password']
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
+class User(AbstractUser):
+    pass
+    # username = models.CharField(max_length=255)
+    # password = models.CharField(max_length=255)
